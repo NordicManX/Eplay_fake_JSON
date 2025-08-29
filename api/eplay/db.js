15a198ab31f1fb7c5e1e5b7c7d50f97750278619
Nelson Antonio de Carvalho Filho <nelsonfilth@hotmail.com>
2025-08-29 18:33:13 -0300

    Create game database in db.js

--- api/eplay/db.js
+++ api/eplay/db.js
@@ -1,1 +1,273 @@
 
+export default {
+  "jogos": [
+    {
+      "id": 1,
+      "category": "action",
+      "title": "Red Dead Redemption 2",
+      "system": "Windows",
+      "description": "Red Dead Redemption 2, a épica aventura de mundo aberto da Rockstar Games aclamada pela crítica e o jogo com maior avaliação da geração de consoles, agora aprimorado para PC com novo conteúdo para o Modo História, melhorias visuais e muito mais.",
+      "infos": ["Ação", "Mundo aberto", "Western"],
+      "image": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png",
+      "media": {
+        "thumbnail": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png",
+        "cover": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png"
+      },
+      "prices": {
+        "old": 299.9,
+        "current": 199.9
+      }
+    },
+    {
+      "id": 2,
+      "category": "action",
+      "title": "The Witcher 3: Wild Hunt",
+      "system": "Windows",
+      "description": "Você é Geralt de Rívia, mercenário matador de monstros. À sua disposição, todas as ferramentas do ofício: espadas afiadas, misturas letais, bestas discretas e magias de combate poderosas.",
+      "infos": ["RPG", "Mundo aberto", "Fantasia"],
+      "image": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2",
+      "media": {
+        "thumbnail": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2",
+        "cover": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2"
+      },
+      "prices": {
+        "old": 129.9,
+        "current": 49.9
+      }
+    },
+    {
+      "id": 3,
+      "category": "fight",
+      "title": "Mortal Kombat 11",
+      "system": "Windows",
+      "description": "Mortal Kombat está de volta, melhor do que nunca, em uma nova evolução da icônica franquia. Todas as variações de customização de personagens lhe dão controle total para personalizar os lutadores e torná-los seus.",
+      "infos": ["Luta", "Ação", "Multijogador"],
+      "image": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png",
+      "media": {
+        "thumbnail": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png",
+        "cover": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png"
+      },
+      "prices": {
+        "old": 199.9,
+        "current": 89.9
+      }
+    },
+    {
+      "id": 4,
+      "category": "rpg",
+      "title": "Cyberpunk 2077",
+      "system": "Windows",
+      "description": "Cyberpunk 2077 é uma história de ação e aventura de mundo aberto ambientada em Night City, uma megalópole obcecada por poder, glamour e modificação corporal.",
+      "infos": ["RPG", "Mundo aberto", "Sci-fi"],
+      "image": "https://cdn-products.eneba.com/cdn-cgi/image/width=352,height=500,quality=75,format=avif/images/products/182650/414271.jpg",
+      "media": {
+        "thumbnail": "https://cdn-products.eneba.com/cdn-cgi/image/width=352,height=500,quality=75,format=avif/images/products/182650/414271.jpg",
+        "cover": "https://cdn-products.eneba.com/cdn-cgi/image/width=352,height=500,quality=75,format=avif/images/products/182650/414271.jpg"
+      },
+      "prices": {
+        "old": 249.9,
+        "current": 199.9
+      }
+    },
+    {
+      "id": 5,
+      "category": "simulation",
+      "title": "Microsoft Flight Simulator",
+      "system": "Windows",
+      "description": "Dos aviões leves aos jatos de fuselagem larga, voe em aeronaves altamente detalhadas e precisas na próxima geração do Microsoft Flight Simulator. Teste suas habilidades de pilotagem contra os desafios do voo noturno, simulação atmosférica em tempo real e clima ao vivo em um mundo dinâmico e vivo.",
+      "infos": ["Simulação", "Voo", "Mundo aberto"],
+      "image": "https://s2.glbimg.com/QhUs3p2i67dv9e11v2v2x2Q3iGA=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/g/b/27b16d3f274a7b4594c351f2e8251e60/microsoft-flight-simulator-captura-de-tela-2020-08-17-18-09-11-20.png",
+      "media": {
+        "thumbnail": "https://s2.glbimg.com/QhUs3p2i67dv9e11v2v2x2Q3iGA=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/g/b/27b16d3f274a7b4594c351f2e8251e60/microsoft-flight-simulator-captura-de-tela-2020-08-17-18-09-11-20.png",
+        "cover": "https://s2.glbimg.com/QhUs3p2i67dv9e11v2v2x2Q3iGA=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/g/b/27b16d3f274a7b4594c351f2e8251e60/microsoft-flight-simulator-captura-de-tela-2020-08-17-18-09-11-20.png"
+      },
+      "prices": {
+        "old": 349.9,
+        "current": 249.9
+      }
+    },
+    {
+      "id": 6,
+      "category": "sport",
+      "title": "FIFA 23",
+      "system": "Playstation 5",
+      "description": "EA SPORTS™ FIFA 23 traz ainda mais da ação e realismo do futebol para dentro de campo no Jogo de Todo Mundo, com avanços na Tecnologia HyperMotion2 que agora tem o dobro de capturas de movimentos do mundo real, criando animações de futebol mais reais do que nunca.",
+      "infos": ["Esporte", "Futebol", "Multijogador"],
+      "image": "https://image.api.playstation.com/vulcan/img/rnd/202208/0410/82l54QAsuIn2Xm2T631qfVMs.png",
+      "media": {
+        "thumbnail": "https://image.api.playstation.com/vulcan/img/rnd/202208/0410/82l54QAsuIn2Xm2T631qfVMs.png",
+        "cover": "https://image.api.playstation.com/vulcan/img/rnd/202208/0410/82l54QAsuIn2Xm2T631qfVMs.png"
+      },
+      "prices": {
+        "old": 339.9,
+        "current": 129.9
+      }
+    }
+  ],
+  "destaque": {
+    "id": 1,
+    "category": "action",
+    "title": "Red Dead Redemption 2",
+    "system": "Windows",
+    "description": "Red Dead Redemption 2, a épica aventura de mundo aberto da Rockstar Games aclamada pela crítica e o jogo com maior avaliação da geração de consoles, agora aprimorado para PC com novo conteúdo para o Modo História, melhorias visuais e muito mais.",
+    "infos": ["Ação", "Mundo aberto", "Western"],
+    "image": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png",
+    "media": {
+      "thumbnail": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png",
+      "cover": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png"
+    },
+    "prices": {
+      "old": 299.9,
+      "current": 199.9
+    }
+  },
+  "promocoes": [
+    {
+      "id": 2,
+      "category": "action",
+      "title": "The Witcher 3: Wild Hunt",
+      "system": "Windows",
+      "description": "Você é Geralt de Rívia, mercenário matador de monstros. À sua disposição, todas as ferramentas do ofício: espadas afiadas, misturas letais, bestas discretas e magias de combate poderosas.",
+      "infos": ["RPG", "Mundo aberto", "Fantasia"],
+      "image": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2",
+      "media": {
+        "thumbnail": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2",
+        "cover": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2"
+      },
+      "prices": {
+        "old": 129.9,
+        "current": 49.9
+      }
+    },
+    {
+      "id": 3,
+      "category": "fight",
+      "title": "Mortal Kombat 11",
+      "system": "Windows",
+      "description": "Mortal Kombat está de volta, melhor do que nunca, em uma nova evolução da icônica franquia. Todas as variações de customização de personagens lhe dão controle total para personalizar os lutadores e torná-los seus.",
+      "infos": ["Luta", "Ação", "Multijogador"],
+      "image": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png",
+      "media": {
+        "thumbnail": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png",
+        "cover": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png"
+      },
+      "prices": {
+        "old": 199.9,
+        "current": 89.9
+      }
+    }
+  ],
+  "em-breve": [],
+  "acao": [
+    {
+      "id": 1,
+      "category": "action",
+      "title": "Red Dead Redemption 2",
+      "system": "Windows",
+      "description": "Red Dead Redemption 2, a épica aventura de mundo aberto da Rockstar Games aclamada pela crítica e o jogo com maior avaliação da geração de consoles, agora aprimorado para PC com novo conteúdo para o Modo História, melhorias visuais e muito mais.",
+      "infos": ["Ação", "Mundo aberto", "Western"],
+      "image": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png",
+      "media": {
+        "thumbnail": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png",
+        "cover": "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/Y02ljdBodKxOUbtiHES1sIM8.png"
+      },
+      "prices": {
+        "old": 299.9,
+        "current": 199.9
+      }
+    },
+    {
+      "id": 2,
+      "category": "action",
+      "title": "The Witcher 3: Wild Hunt",
+      "system": "Windows",
+      "description": "Você é Geralt de Rívia, mercenário matador de monstros. À sua disposição, todas as ferramentas do ofício: espadas afiadas, misturas letais, bestas discretas e magias de combate poderosas.",
+      "infos": ["RPG", "Mundo aberto", "Fantasia"],
+      "image": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2",
+      "media": {
+        "thumbnail": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2",
+        "cover": "https://cdn1.epicgames.com/offer/1241c240157e413992647369a4123612/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf45a33568b534a6d4129c855e2"
+      },
+      "prices": {
+        "old": 129.9,
+        "current": 49.9
+      }
+    }
+  ],
+  "esportes": [
+    {
+      "id": 6,
+      "category": "sport",
+      "title": "FIFA 23",
+      "system": "Playstation 5",
+      "description": "EA SPORTS™ FIFA 23 traz ainda mais da ação e realismo do futebol para dentro de campo no Jogo de Todo Mundo, com avanços na Tecnologia HyperMotion2 que agora tem o dobro de capturas de movimentos do mundo real, criando animações de futebol mais reais do que nunca.",
+      "infos": ["Esporte", "Futebol", "Multijogador"],
+      "image": "https://image.api.playstation.com/vulcan/img/rnd/202208/0410/82l54QAsuIn2Xm2T631qfVMs.png",
+      "media": {
+        "thumbnail": "https://image.api.playstation.com/vulcan/img/rnd/202208/0410/82l54QAsuIn2Xm2T631qfVMs.png",
+        "cover": "https://image.api.playstation.com/vulcan/img/rnd/202208/0410/82l54QAsuIn2Xm2T631qfVMs.png"
+      },
+      "prices": {
+        "old": 339.9,
+        "current": 129.9
+      }
+    }
+  ],
+  "simulacao": [
+    {
+      "id": 5,
+      "category": "simulation",
+      "title": "Microsoft Flight Simulator",
+      "system": "Windows",
+      "description": "Dos aviões leves aos jatos de fuselagem larga, voe em aeronaves altamente detalhadas e precisas na próxima geração do Microsoft Flight Simulator. Teste suas habilidades de pilotagem contra os desafios do voo noturno, simulação atmosférica em tempo real e clima ao vivo em um mundo dinâmico e vivo.",
+      "infos": ["Simulação", "Voo", "Mundo aberto"],
+      "image": "https://s2.glbimg.com/QhUs3p2i67dv9e11v2v2x2Q3iGA=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/g/b/27b16d3f274a7b4594c351f2e8251e60/microsoft-flight-simulator-captura-de-tela-2020-08-17-18-09-11-20.png",
+      "media": {
+        "thumbnail": "https://s2.glbimg.com/QhUs3p2i67dv9e11v2v2x2Q3iGA=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/g/b/27b16d3f274a7b4594c351f2e8251e60/microsoft-flight-simulator-captura-de-tela-2020-08-17-18-09-11-20.png",
+        "cover": "https://s2.glbimg.com/QhUs3p2i67dv9e11v2v2x2Q3iGA=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/g/b/27b16d3f274a7b4594c351f2e8251e60/microsoft-flight-simulator-captura-de-tela-2020-08-17-18-09-11-20.png"
+      },
+      "prices": {
+        "old": 349.9,
+        "current": 249.9
+      }
+    }
+  ],
+  "luta": [
+    {
+      "id": 3,
+      "category": "fight",
+      "title": "Mortal Kombat 11",
+      "system": "Windows",
+      "description": "Mortal Kombat está de volta, melhor do que nunca, em uma nova evolução da icônica franquia. Todas as variações de customização de personagens lhe dão controle total para personalizar os lutadores e torná-los seus.",
+      "infos": ["Luta", "Ação", "Multijogador"],
+      "image": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png",
+      "media": {
+        "thumbnail": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png",
+        "cover": "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/itb2RIklp3a9A243A1cM1a2N.png"
+      },
+      "prices": {
+        "old": 199.9,
+        "current": 89.9
+      }
+    }
+  ],
+  "rpg": [
+    {
+      "id": 4,
+      "category": "rpg",
+      "title": "Cyberpunk 2077",
+      "system": "Windows",
+      "description": "Cyberpunk 2077 é uma história de ação e aventura de mundo aberto ambientada em Night City, uma megalópole obcecada por poder, glamour e modificação corporal.",
+      "infos": ["RPG", "Mundo aberto", "Sci-fi"],
+      "image": "https://cdn-products.eneba.com/cdn-cgi/image/width=352,height=500,quality=75,format=avif/images/products/182650/414271.jpg",
+      "media": {
+        "thumbnail": "https://cdn-products.eneba.com/cdn-cgi/image/width=352,height=500,quality=75,format=avif/images/products/182650/414271.jpg",
+        "cover": "https://cdn-products.eneba.com/cdn-cgi/image/width=352,height=500,quality=75,format=avif/images/products/182650/414271.jpg"
+      },
+      "prices": {
+        "old": 249.9,
+        "current": 199.9
+      }
+    }
+  ]
+};
